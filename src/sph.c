aaade424a3c00d237010376828145866c9314076
Mingli Yuan <mingli.yuan@gmail.com>
2011-12-30 23:20:32 +0800

    change style

--- src/sph.c
+++ src/sph.c
@@ -9,18 +9,12 @@ int main(int argc, const char **argv)
         return execv("sph-help", nargv);
     } {
         const char* cmd = argv[1];
-        if (strcmp(cmd, "cli") == 0)
-            return execv("sph-cli", nargv);
-        if (strcmp(cmd, "help") == 0)
-            return execv("sph-help", nargv);
-        if (strcmp(cmd, "init") == 0)
-            return execv("sph-init", nargv);
-        if (strcmp(cmd, "part") == 0)
-            return execv("sph-part", nargv);
-        if (strcmp(cmd, "start") == 0)
-            return execv("sph-start", nargv);
-        if (strcmp(cmd, "stop") == 0)
-            return execv("sph-stop", nargv);
+        if (strcmp(cmd, "cli") == 0)   return execv("sph-cli", nargv);
+        if (strcmp(cmd, "help") == 0)  return execv("sph-help", nargv);
+        if (strcmp(cmd, "init") == 0)  return execv("sph-init", nargv);
+        if (strcmp(cmd, "part") == 0)  return execv("sph-part", nargv);
+        if (strcmp(cmd, "start") == 0) return execv("sph-start", nargv);
+        if (strcmp(cmd, "stop") == 0)  return execv("sph-stop", nargv);
         printf("unsupported command: %s\n", cmd);
     }
 

ddde313bfa7d2a3d6ae77230c7ddfcea2fcd96e8
Mingli Yuan <mingli.yuan@gmail.com>
2012-02-27 01:19:46 +0800

    add tests for vector fields

--- sphere/test/test_field.h
+++ sphere/test/test_field.h
@@ -7,7 +7,7 @@ class SuiteField : public CxxTest::TestSuite
 {
 
 public:
-    void test_constructor(void)
+    void test_scalar_constructor(void)
     {
         ScalarField field = ScalarField(0, 1);
         TS_ASSERT_EQUALS(field[0], 1);
@@ -24,7 +24,7 @@ public:
         TS_ASSERT_EQUALS(field[11], 1);
     }
 
-    void test_bracket_lhs(void)
+    void test_scalar_bracket_lhs(void)
     {
         ScalarField field = ScalarField(0, 1);
         field[3] = 3;
@@ -44,7 +44,7 @@ public:
         TS_ASSERT_EQUALS(field[11], 1);
     }
 
-    void test_assignment(void)
+    void test_scalar_assignment(void)
     {
         ScalarField field1 = ScalarField(0, 1);
         ScalarField field2 = ScalarField(0, 2);
@@ -78,7 +78,7 @@ public:
         TS_ASSERT_EQUALS(field2[11], 2);
     }
 
-    void test_add(void)
+    void test_scalar_add(void)
     {
         ScalarField field = ScalarField(0, 1);
         ScalarField field1 = ScalarField(0, 2);
@@ -97,7 +97,7 @@ public:
         TS_ASSERT_EQUALS(field[11], 3);
     }
 
-    void test_minus(void)
+    void test_scalar_minus(void)
     {
         ScalarField field = ScalarField(0, 5);
         ScalarField field1 = ScalarField(0, 2);
@@ -116,7 +116,7 @@ public:
         TS_ASSERT_EQUALS(field[11], 3);
     }
 
-    void test_product(void)
+    void test_scalar_product(void)
     {
         ScalarField field = ScalarField(0, 1);
         field *= 3;
@@ -134,7 +134,7 @@ public:
         TS_ASSERT_EQUALS(field[11], 3);
     }
 
-    void test_divid(void)
+    void test_scalar_divid(void)
     {
         ScalarField field = ScalarField(0, 6);
         field /= 2;
@@ -150,5 +150,223 @@ public:
         TS_ASSERT_EQUALS(field[9], 3);
         TS_ASSERT_EQUALS(field[10], 3);
         TS_ASSERT_EQUALS(field[11], 3);
+    }
+
+    void test_vector_add(void)
+    {
+        VectorField field = VectorField(0, new Vector(1,1,1));
+        VectorField field1 = VectorField(0, new Vector(2,2,2));
+        field += field1;
+        Vector vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[1];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[2];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[3];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[4];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[5];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[6];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[7];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[8];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[9];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[10];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+    }
+
+    void test_vector_minus(void)
+    {
+        VectorField field = VectorField(0, new Vector(5, 5, 5));
+        VectorField field1 = VectorField(0, new Vector(2, 2, 2));
+        field -= field1;
+        Vector vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[1];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[2];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[3];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[4];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[5];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[6];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[7];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[8];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[9];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[10];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+    }
+
+    void test_vector_product(void)
+    {
+        VectorField field = VectorField(0, new Vector(1, 1, 1));
+        field *= 3;
+        Vector vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[1];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[2];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[3];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[4];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[5];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[6];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[7];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[8];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[9];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[10];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+    }
+
+    void test_vector_divid(void)
+    {
+        VectorField field = VectorField(0, new Vector(6, 6, 6));
+        field /= 2;
+        Vector vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[1];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[2];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[3];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[4];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[5];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[6];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[7];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[8];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[9];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[10];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
+        vect = field[0];
+        TS_ASSERT_EQUALS(vect.x, 3);
+        TS_ASSERT_EQUALS(vect.y, 3);
+        TS_ASSERT_EQUALS(vect.z, 3);
     }
 };
